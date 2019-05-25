06716b8c0d320352c138b9f2984883d6417ef3f8
lunitin <dinsmorc@oregonstate.edu>
2019-05-25 12:16:14 -0700

    Added basic node server, custom error handler, and simple auth & validation

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const CustomError = require("./lib/custom-error");
+
+const api = require('./api');
+
+const app = express();
+const port = process.env.PORT || 8000;
+
+
+/*
+ * Morgan is a popular logger.
+ */
+app.use(morgan('dev'));
+
+app.use(bodyParser.json());
+
+/*
+ * All routes for the API are written in modules in the api/ directory.  The
+ * top-level router lives in api/index.js.  That's what we include here, and
+ * it provides all of the routes.
+ */
+app.use('/', api);
+
+/*
+ * Default error handling route
+ */
+app.use('*', (err, req, res, next) => {
+  console.error(err);
+  res.status(err.code).send({
+    error: err.message
+  });
+});
+
+app.use('*', function (req, res, next) {
+  res.status(404).json({
+    error: "Requested resource " + req.originalUrl + " does not exist"
+  });
+});
+
+app.listen(port, function() {
+  console.log("== Server is running on port", port);
+});
