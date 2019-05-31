ed6270b4dedacea747da6d75c98f7f2da44ffc2f
waskod <waskod@oregonstate.edu>
2019-05-30 17:53:46 -0700

    Update course.js

--- models/course.js
+++ models/course.js
@@ -8,6 +8,9 @@
 const { extractValidFields } = require('../lib/validation');
 const CustomError = require("../lib/custom-error");
 
+//
+// NOTE: I still need to test the functions that are written
+//
 
 /*
  * Schema describing required/optional fields of a Course object.
@@ -37,8 +40,27 @@ exports.RosterSchema = RosterSchema;
  */
 exports.getCourses = async (page, subject, number, term) => {
   console.log(" == getCourses: page,subject,number,term", page,subject,number,term);
+  const db = getDBReference();
+  const collection = db.collection('courses');
+  const courseCount = await collection.countDocuments();
+  const numPerPage = 5;
+  const lastPage = Math.ceil(courseCount / numPerPage);
+  page = page < 1 ? 1 : page;
+  page = page > lastPage ? lastPage : page;
+  const offset = (page - 1) * numPerPage;
+  const results = await collection.find({})
+    .sort({_id: 1})
+    .skip(offset)
+    .limit(numPerPage)
+    .toArray();
 
-  return {};
+  return {
+    courses: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: numPerPage,
+    count: count
+  };
 };
 
 
@@ -47,8 +69,10 @@ exports.getCourses = async (page, subject, number, term) => {
  */
 exports.insertCourse = async (course) => {
   console.log(" == insertCourse: course", course);
-
-  return "123";
+  const db = getDBReference();
+  const collection = db.collection('courses');
+  const result = await collection.insertOne(course);
+  return result.insertedId;
 };
 
 
@@ -57,8 +81,10 @@ exports.insertCourse = async (course) => {
  */
 exports.getCourseDetailsById = async (id) => {
   console.log(" == getCourseDetailsById: id", id);
-
-  return {};
+  const db = getDBReference();
+  const collection = db.collection('courses');
+  const results = await collection.find({id}).toArray();
+  return results[0];
 };
 
 
@@ -67,8 +93,26 @@ exports.getCourseDetailsById = async (id) => {
  */
 exports.updateCourse = async (id, course) => {
   console.log(" == updateCourse: course", course);
-
-  return true;
+  return new Promise((resolve, reject) =>{
+    const db = getDBReference();
+    db.collection('courses', function(err, collection) {
+      collection.updateOne({_id: new mongodb.ObjectID(id)}, {
+        subject: course.subject,
+        number: course.number,
+        title: course.title,
+        term: course.term,
+        instructorId: course.insertedId
+      }), function(err, results) {
+        if(err)
+        {
+          console.log(err);
+          reject(err);
+        }
+        console.log("Successfully deleted course id: "+id);
+        resolve(results);
+      }
+    });
+  });
 };
 
 
@@ -77,8 +121,21 @@ exports.updateCourse = async (id, course) => {
  */
 exports.deleteCourse = async (id) => {
   console.log(" == deleteCourse: id", id);
-
-  return true;
+  return new Promise((resolve, reject) =>{
+    const db = getDBReference();
+    db.collection('courses', function(err, collection) {
+      collection.deleteOne({_id: new mongodb.ObjectID(id)}), function(err, results) {
+        if(err)
+        {
+          console.log(err);
+          reject(err);
+        }
+        console.log("Successfully deleted course id: "+id);
+        resolve(results);
+      }
+    });
+  });
+  
 };
 
 
@@ -107,8 +164,27 @@ exports.updateCourseRoster = async (course, updates) => {
  */
 exports.getCourseAssignments = async (course) => {
   console.log(" == getCourseAssignments: course", course);
+  const db = getDBReference();
+  const collection = db.collection('assignments');
+  const assignmentCount = await collection.find({courseId: course.id}).countDocuments();
+  const numPerPage = 5;
+  const lastPage = Math.ceil(assignmentCount / numPerPage);
+  page = page < 1 ? 1 : page;
+  page = page > lastPage ? lastPage : page;
+  const offset = (page - 1) * numPerPage;
+  const results = await collection.find({courseId: course.id})
+    .sort({_id: 1})
+    .skip(offset)
+    .limit(numPerPage)
+    .toArray();
 
-  return {};
+  return {
+    assignments: results,
+    page: page,
+    totalPages: lastPage,
+    pageSize: numPerPage,
+    count: count
+  };
 };
 
 
@@ -117,16 +193,26 @@ exports.getCourseAssignments = async (course) => {
  */
 exports.instructorOwnsCourse = async (instructor, course) => {
   console.log(" == instructorOwnsCourse: instructor, course", instructor, course);
-
-  return true;
+  return new Promise((resolve, reject) => {
+    const db = getDBReference();
+    const collection = db.collection('courses');
+    const results = await collection.find({iinstructorId: instructor.id}).toArray();
+    if(results.length == 0)
+      reject();
+    resolve (results[0]);
+  });
 };
 
 
 /*
  * Check if a student is enrolled in the specified Course Id
  */
+
+ // I think we are going to need to store students enrolled in a course
+ // in the courses collection as an array of id's or something
 exports.studentInCourse = async (student, course) => {
   console.log(" == studentInCourse: student, course", student, course);
-
-  return true;
+  return new Promise((resolve, reject) => {
+    // Need to make a solution for searching students first.
+  });
 };
