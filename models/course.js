101d744b698182d09cf4dabe70d4cd0a5980cdb7
waskod <waskod@oregonstate.edu>
2019-06-07 21:31:55 -0700

    Re-Work Student into separate table & schema inside course

--- models/course.js
+++ models/course.js
@@ -7,9 +7,13 @@
 
 const { extractValidFields } = require('../lib/validation');
 const CustomError = require("../lib/custom-error");
-
+const { Parser } = require('json2csv');
 
 const { getDBReference, _ID } = require ('../lib/mongoDB');
+
+const {
+  getUserDetailsById,
+} = require('../models/user');
 
 //
 // NOTE: Still need to do the two roster functions and then test my changes
@@ -24,9 +28,6 @@ const CourseSchema = {
  title: { required: true },
  term: { required: true },
  instructorId: { required: true },
- students: {required: false}
- // list of students
- // list of assignments
 };
 exports.CourseSchema = CourseSchema;
 
@@ -40,6 +41,13 @@ const RosterSchema = {
 };
 exports.RosterSchema = RosterSchema;
 
+
+const StudentSchema = {
+  studentId: {required: true},
+  courseId: {required: true}
+};
+exports.StudentSchema = StudentSchema;
+
 // Variable to control page size
 const PAGE_SIZE = 3;
 
@@ -51,7 +59,7 @@ const PAGE_SIZE = 3;
 exports.getCourses = async (page, subject, number, term) => {
   try
   {
-    console.log(" == getCourses: page,subject,number,term", page,subject,number,term);
+    console.log(` == getCourses: ${page}, ${subject}, ${number}, ${term}`);
     const collection = getDBReference().collection('courses');
     const courseCount = await collection.find({ $and: [ {subject: subject}, {number: number}, {term: term}]}).countDocuments();
     const PAGE_SIZE = 5;
@@ -64,51 +72,79 @@ exports.getCourses = async (page, subject, number, term) => {
       .skip(offset)
       .limit(PAGE_SIZE)
       .toArray();
+
+    const pagLinks = {};
+    if(page < lastPage) 
+    {
+      pagLinks.nextPage = `/courses?page=${page + 1}`;
+      pagLinks.lastPage = `/courses?page=${lastPage}`;
+    }
+    if(page > 1) 
+    {
+      pagLinks.prevPage = `/courses?page=${page - 1}`;
+      pagLinks.firstPage = '/courses?page=1';
+    }
   
     return {
       courses: results,
       page: page,
       totalPages: lastPage,
       pageSize: PAGE_SIZE,
-      count: count
+      count: count,
+      links: pagLinks
     };
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  getCourses()");
+    console.log(` !!! Error in /models/courses.js  :  getCourses()`);
     console.log(err);
   }
 
 };
 
 exports.getAllCourses = async (page) => {
-  console.log(" == Getting All Courses");
+  console.log(` == Getting All Courses`);
   try
   {
-    console.log(" == getCourses: page,subject,number,term", page,subject,number,term);
+    console.log(` == getCourses: ${page}, ${subject}, ${number}, ${term}`);
     const collection = getDBReference().collection('courses');
     const courseCount = await collection.countDocuments();
     const lastPage = Math.ceil(courseCount / PAGE_SIZE);
     page = page < 1 ? 1 : page;
     page = page > lastPage ? lastPage : page;
     const offset = (page - 1) * PAGE_SIZE;
+    var end = start + pageSize;
+    end = end > results.length ? results.length : end;
     const results = await collection.find({})
       .sort({_id: 1})
       .skip(offset)
       .limit(PAGE_SIZE)
       .toArray();
+
+    const pagLinks = {};
+    if(page < lastPage) 
+    {
+      pagLinks.nextPage = `/courses?page=${page + 1}`;
+      pagLinks.lastPage = `/courses?page=${lastPage}`;
+    }
+    if(page > 1) 
+    {
+      pagLinks.prevPage = `/courses?page=${page - 1}`;
+      pagLinks.firstPage = '/courses?page=1';
+    }
   
     return {
       courses: results,
       page: page,
       totalPages: lastPage,
       pageSize: PAGE_SIZE,
-      count: count
+      count: count,
+      links: pagLinks
     };
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  getAllCourses()");
+    console.log(` !!! Error in /models/courses.js  :  getAllCourses()`);
     console.log(err);
   }
 };
@@ -120,17 +156,17 @@ exports.getAllCourses = async (page) => {
 exports.insertCourse = async (course) => {
   try
   {
-    console.log(" == Inserting Course:", course);
+    console.log(` == Inserting Course: ${course}`);
     const courseFields = extractValidFields(course, CourseSchema);
     const collection = getDBReference().collection('courses');
     const result = await collection.insertOne(courseFields);
     console.log(result);
-    console.log(" == Course Inserted With ID: ", result.insertedId);
+    console.log(` == Course Inserted With ID: `, result.insertedId);
     return result.insertedId;
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  insertCourse()");
+    console.log(` !!! Error in /models/courses.js  :  insertCourse()`);
     console.log(err);
   }
   
@@ -143,14 +179,19 @@ exports.insertCourse = async (course) => {
 exports.getCourseDetailsById = async (id) => {
   try
   {
-    console.log(" == Getting Course Details By ID: ", id);
+    console.log(` == Getting Course Details By ID: ${id}`);
+    if(!_ID.isValid(id))
+    {
+      console.log(` == Invalid ID in getCourseDetailsById: `, id);
+      return null;
+    }
     const collection = getDBReference().collection('courses');
     var result = await collection.findOne({ _id: new _ID(id)});
     return result;
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  getCourseDetailsById()");
+    console.log(` !!! Error in /models/courses.js  :  getCourseDetailsById()`);
     console.log(err);
   }
 };
@@ -162,19 +203,19 @@ exports.getCourseDetailsById = async (id) => {
 exports.updateCourse = async (id, patch) => {
   try
   {
-    console.log(" == Updating Course ID: ", id);
+    console.log(` == Updating Course ID: ${id}`);
     const collection = getDBReference.collection('courses');
     var result = await collection.findOneAndUpdate(
       { _id: new _ID(id)},
       {$set: patch},
       {returnNewDocument:true},
     );
-    console.log(" == Updated Course:", result);
+    console.log(` == Updated Course:`, result);
     return result;
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  updateCourse()");
+    console.log(` !!! Error in /models/courses.js  :  updateCourse()`);
     console.log(err);
   }
 
@@ -187,21 +228,21 @@ exports.updateCourse = async (id, patch) => {
 exports.deleteCourse = async (id) => {
   try
   {
-    console.log(" == Deleting Course ID: ", id);
+    console.log(` == Deleting Course ID: ${id}`);
     const collection = getDBReference().collection('courses');
     var result = await collection.remove(
       { _id: new _ID(id)},
       { justOne: true},
     );
-    console.log(" == Deleted Assignment ID: ${id}, Result:${result.result}");
+    console.log(` == Deleted Assignment ID: ${id}, Result:${result.result}`);
     if(result.result.n > 0)
     {
       return result.result;
     }
     else
     {
-      console.log(" !!! Error in /models/courses.js  :  deleteCourse()");
-      throw new CustomError("Delete Course Error, No Record For Course ID:${id}", 404);
+      console.log(` !!! Error in /models/courses.js  :  deleteCourse()`);
+      throw new CustomError(`Delete Course Error, No Record For Course ID:${id}`, 404);
     }
   }
   catch(err)
@@ -217,7 +258,7 @@ exports.deleteCourse = async (id) => {
 exports.getCourseRoster = async (courseId) => {
   try
   {
-    console.log(" == Fetching Roster For Course ID:${course.id}");
+    console.log(` == Fetching Roster For Course ID:${course.id}`);
     const collection = getDBReference().collection('courses');
     const course = await collection.findOne({ _id: new _ID(courseId)});
     var i;
@@ -232,7 +273,7 @@ exports.getCourseRoster = async (courseId) => {
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  getCourseRoster()");
+    console.log(` !!! Error in /models/courses.js  :  getCourseRoster()`);
     console.log(err);
   }
 };
@@ -244,7 +285,7 @@ exports.getCourseRoster = async (courseId) => {
 exports.updateCourseRoster = async (courseId, roster) => {
   try
   {
-    console.log(" == Updating Roster For Course ID:${course.id}", roster);
+    console.log(` == Updating Roster For Course ID:${course.id} Roster: ${roster}`);
     const addStudents = roster.add;
     const remStudents = roster.remove;
     var i;
@@ -259,7 +300,7 @@ exports.updateCourseRoster = async (courseId, roster) => {
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  updateCourseRoster()");
+    console.log(` !!! Error in /models/courses.js  :  updateCourseRoster()`);
     console.log(err);
   }
 };
@@ -272,10 +313,10 @@ exports.getCourseAssignments = async (id, page) => {
 
   try
   {
-    console.log(" == Getting Assignments For Course ID:", id);
+    console.log(` == Getting Assignments For Course ID: ${id}`);
     const collection = getDBReference().collection('assignments');
     const assignmentCount = await collection.find({courseId: course.id}).countDocuments();
-    console.log(" == Reading ${assignmentCount} Assignments for Course ID: ", id);
+    console.log(` == Reading ${assignmentCount} Assignments for Course ID: `, id);
     const lastPage = Math.ceil(assignmentCount / PAGE_SIZE);
     page = page < 1 ? 1 : page;
     page = page > lastPage ? lastPage : page;
@@ -297,7 +338,7 @@ exports.getCourseAssignments = async (id, page) => {
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  getCourseAssignments()");
+    console.log(` !!! Error in /models/courses.js  :  getCourseAssignments()`);
     console.log(err);
   }
 };
@@ -309,7 +350,7 @@ exports.getCourseAssignments = async (id, page) => {
 exports.instructorOwnsCourse = async (instructor, course) => {
   try
   {
-    console.log(" == Checking if Instructor ID:${instructor.id} owns Course ID: ${course.id}");
+    console.log(` == Checking if Instructor ID:${instructor.id} owns Course ID: ${course.id}`);
     const collection = getDBReference().collection('courses');
     const results = await collection.find(
       {
@@ -330,7 +371,7 @@ exports.instructorOwnsCourse = async (instructor, course) => {
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  instructorOwnsCourse()");
+    console.log(` !!! Error in /models/courses.js  :  instructorOwnsCourse()`);
     console.log(err);
   }
 };
@@ -341,7 +382,7 @@ exports.instructorOwnsCourse = async (instructor, course) => {
 exports.instructorIdOwnsCourseId = async (instructorId, courseId) => {
   try
   {
-    console.log(" == Checking if Instructor ID:${instructor.id} owns Course ID: ${course.id}");
+    console.log(` == Checking if Instructor ID:${instructor.id} owns Course ID: ${course.id}`);
     const collection = getDBReference().collection('courses');
     const results = await collection.find(
       {
@@ -361,61 +402,33 @@ exports.instructorIdOwnsCourseId = async (instructorId, courseId) => {
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  instructorIdOwnsCourseId()");
+    console.log(` !!! Error in /models/courses.js  :  instructorIdOwnsCourseId()`);
     console.log(err);
   }
 };
 
-
-
-
-/*
- * Check if a student is enrolled in the specified Course Id
- */
-
- // I think we are going to need to store students enrolled in a course
- // in the courses collection as an array of id's or something
-exports.studentInCourse = async (user, course) => {
-  try
-  {
-    console.log(" == Checking if Student ID:${user.id} is Enrolled In Course ID:${course.id}");
-    const collection = getDBReference().collection('courses');
-    const course = await collection.findOne({ _id: new _ID(course.id)});
-    var i;
-    var students = course.students;
-    for(i = 0; i < students.length; i++)
-    {
-      if(students[i] == user.id)
-        foundStudent = true;
-    }
-    return foundStudent;
-  }
-  catch(err)
-  {
-    console.log(" !!! Error in /models/courses.js  :  studentInCourse()");
-    console.log(err);
-  }
-};
 
 
 exports.studentIdInCourseId = async (studentId, courseId) => {
   try
   {
-    console.log(" == Checking if Student ID:${studentId} is Enrolled In Course ID:${courseId}");
-    const collection = getDBReference().collection('courses');
-    const course = await collection.findOne({ _id: new _ID(courseId)});
-    var i;
-    var students = course.students;
-    for(i = 0; i < students.length; i++)
-    {
-      if(students[i] == studentId)
-        foundStudent = true;
-    }
-    return foundStudent;
+    console.log(` == Checking if Student ID:${studentId} is Enrolled In Course ID:${courseId}`);
+    const collection = getDBReference().collection('students');
+    const student = await collection.findOne(
+      { 
+        $and: [ 
+          {studentId: studentId}, 
+          {courseId: courseId}, 
+        ]
+      }
+    );
+    if(student)
+      return true;
+    return false;
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  studentIdInCourseId()");
+    console.log(` !!! Error in /models/courses.js  :  studentIdInCourseId()`);
     console.log(err);
   }
 };
@@ -424,76 +437,118 @@ exports.studentIdInCourseId = async (studentId, courseId) => {
 exports.addStudentToCourse = async (studentId, courseId) => {
   try
   {
-    console.log(" == Adding Student ID:${user.id} to Course ID:${courseId}");
-    var studentInClass = await this.studentIdInCourseId(studentId, courseId);
-    if(studentInClass)
-    {
-      console.log("Add Student To Course Failure, Student ID:${studentId} is Already Enrolled In Course ID:${courseId}");
-      return false;
-    }
-    const collection = getDBReference().collection('courses');
-    const course = await collection.findOne({ _id: new _ID(courseId)});
-    var students = course.students;
-    if(students)
-    {
-      students.push(studentId);
-    }
-    else
-    {
-      students = [];
-      students.push(studentId);
-    }
-    var updatedCourse = await collection.findOneAndUpdate(
-      { _id: new _ID(courseId)},
-      {students: students},
-      {returnNewDocument: true}
-    );
-    
-    console.log(" == Added Student ID:${studentId} to Course ID:${courseId}");
-    return updatedCourse;
+    const collection = getDBReference().collection('students');
+    const usr = await getUserDetailsById(studentId); 
+    const student = {
+      studentId: new _ID(usr.id),
+      courseId: courseId
+    };
+    const result = await collection.insertOne(student);
 
-    
+    console.log(` == Added Student ID:${studentId} to Course ID:${courseId}`);
+    return result.insertedId;
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  addStudentToCourse()");
+    console.log(` !!! Error in /models/courses.js  :  addStudentToCourse()`);
     console.log(err);
   }
 };
+
+exports.getStudentsCSV = async (courseId) =>
+{
+  try
+  {
+    console.log(` == Getting CSV for Course ID: ${courseId}`);
+    const collection = getDBReference().collection('students');
+    const students = collection.find({courseId: new _ID(courseId)}).toArray();
+    var i;
+    var results = [];
+    for(i = 0; i < students.length; i++)
+    {
+      const usr = await getUserDetailsById(students[i].studentId);
+      results[i] = usr;
+    }
+    const fields = ['_id', 'name', 'email', 'password', 'role'];
+    const jsonParser = new Parser({fields});
+    const csv = jsonParser.parse(results);
+    return csv;
+  }
+  catch(err)
+  {
+    console.log(` !!! Error in /models/courses.js  :  getStudentsCSV()`);
+    console.log(err);
+  }
+}
 
 
 exports.removeStudentFromCourse = async (studentId, courseId) => {
   try
   {
-    console.log(" == Removing Student ID:${studentId} from Course ID:${courseId}");
-    var studentInCourse = await this.studentIdInCourseId(studentId, courseId);
-    if(studentInCourse == false)
-    {
-      console.log("Remove Student From Course Failure, Student ID:${studentId} is Already Enrolled In Course ID:${courseId}");
-      return false;
-    }
-    const collection = getDBReference().collection('courses');
-    const course = await collection.findOne({ _id: new _ID(courseId)});
-    var i;
-    var newStudents = [];
-    var students = course.students;  
-    for(i = 0; i < students.length; i++)
-    {
-      if(students[i] != studentId)
-        newStudents.push(students[i]);
-    }
-    var updatedCourse = await collection.findOneAndUpdate(
-      { _id: new _ID(courseId)},
-      {students: newStudents},
-      {returnNewDocument: true}
+    console.log(` == Removing Student ID:${studentId} from Course ID:${courseId} `);
+    const collection = getDBReference().collection('students');
+    var result = await collection.remove(
+      { 
+        $and: [ 
+          {studentId: studentId}, 
+          {courseId: courseId}, 
+        ]
+      }
     );
-    console.log(" == Removed Student ID:${studentId} to Course ID:${courseId}");
-    return updatedCourse;
-
+    return result;
   }
   catch(err)
   {
-    console.log(" !!! Error in /models/courses.js  :  removeStudentFromCourse()");
+    console.log(` !!! Error in /models/courses.js  :  removeStudentFromCourse()`);
     console.log(err);
   }
 };
+
+
+exports.getStudentsInCourse = async (courseId, page) =>
+{
+  try
+  {
+    console.log(` == Getting List Of Students In Course ID:${courseId} `);
+    const collection = getDBReference().collection('students');
+    const count = await collection.find({courseId: new _ID(courseId)}).countDocuments();
+  
+    const lastPage = Math.ceil(count / PAGE_SIZE);
+    page = page < 1 ? 1 : page;
+    page = page > lastPage ? lastPage : page;
+    const start = (page - 1) * PAGE_SIZE;
+  
+    const results = await collection.find({courseId: new _ID(courseId)})
+      .sort({ _id: 1 })
+      .skip(start)
+      .limit(PAGE_SIZE)
+      .toArray();
+  
+    const pagLinks = {};
+  
+    if (page < lastPage) {
+      pagLinks.nextPage = `/students?page=${page + 1}`;
+      pagLinks.lastPage = `/students?page=${lastPage}`;
+    }
+    if (page > 1) {
+      pagLinks.prevPage = `/students?page=${page - 1}`;
+      pagLinks.firstPage = '/students?page=1';
+    }
+  
+    return {
+      students: results,
+      page: page,
+      totalPages: lastPage,
+      pageSize: PAGE_SIZE,
+      count: count,
+      links: pagLinks
+    };
+    
+  }
+  catch(err)
+  {
+    console.log(` !!! Error in /models/courses.js  :  getStudentsInCourse()`);
+    console.log(err);
+  }
+ 
+}
